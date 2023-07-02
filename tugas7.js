83ccfc72ad51135b360c897a86c9dc6a08e444d8
febri <fitrafilthmongers@gmail.com>
2023-07-02 22:17:33 +0700

    tugas 7 js
    mengumpulkan tugas 7 js

---
+++ tugas7.js
@@ -0,0 +1,10 @@
+var tinggibadan = ['150cm','166cm','170cm','145cm','160cm','158cm','168cm']
+
+for(var tinggi of tinggibadan){
+    console.log(tinggi)
+}
+console.log('')
+
+for(x=0;x<tinggibadan.length;x++){
+    console.log(tinggibadan[x])
+}
